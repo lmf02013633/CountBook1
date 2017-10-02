6cac5b7ed3eb08f09460dfb6b4ea1f2a9165d3b0
lmf02013633 <daocaorenatm@hotmail.com>
2017-10-02 15:40:42 +0800

    UPLOAD

--- app/src/main/java/com/example/minfeng1_countbook/MainActivity.java
+++ app/src/main/java/com/example/minfeng1_countbook/MainActivity.java
@@ -8,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+/**
+ * MainActivity
+ * used to add counter to list and give out the number
 
+ */
 public class MainActivity extends AppCompatActivity {
     int count = 0;
     @Override
@@ -16,10 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * onClick
+     * @param view
+     */
     public void onClick(View view){
         count++;
             TextView text=(TextView)findViewById(R.id.textView);
             text.setText("Number of counters: "+ count);
     }
 }
+/*
+Actuallt I also wrote some other classes to try to implement each counter but it doesn't work,
+and when I keep them, the APP keep having error and cannot run, so at last I decided to delete them.
 
+ */
